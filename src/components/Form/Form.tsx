fdb297dbed695b635367193d68f575fe9d991100
Natalia Grishina <tata@MacBook-Air-Natalia.local>
2025-05-12 12:21:49 +0300

    feat: сверстала компонент Form для различных форм приложения

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,23 @@
+import type { FormHTMLAttributes, ReactNode } from "react";
+import styles from "./form.module.scss";
+import clsx from "clsx";
+
+interface IFormProps extends FormHTMLAttributes<HTMLFormElement> {
+	children: ReactNode;
+	isLoading?: boolean;
+	formRef?: React.Ref<HTMLFormElement>;
+}
+
+const Form = ({ formRef, children, isLoading, ...props }: IFormProps) => {
+	return (
+    <form
+      ref={formRef}
+			className={clsx(styles.form, isLoading && styles.formLoading)}
+			{...props}
+		>
+			{children}
+		</form>
+	);
+};
+
+export default Form;
