78dacac6626773ee1688958fc0eb2bd4b4e05a6c
Natalia Grishina <tata@MacBook-Air-Natalia.local>
2025-05-13 14:24:21 +0300

    feat: добавила инициализацию React Query и Toaster

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,11 +4,17 @@ import "../styles/index.scss";
 import App from "./App";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../store/api/queryClient";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<ErrorBoundary FallbackComponent={ErrorPage}>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<Toaster/>
+			</QueryClientProvider>
 		</ErrorBoundary>
 	</StrictMode>
 );
