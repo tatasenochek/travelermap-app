c2e98a4f63e3173d1711a5e2bdcf0ce78b6486d7
Natalia Grishina <tata@MacBook-Air-Natalia.local>
2025-05-13 18:18:24 +0300

    feat: добавила </PrivateRoute> для защиты маршрутов

---
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,24 @@
+import { Navigate } from "react-router-dom";
+import { ROUTES } from "../../router/ROUTES";
+import type { ReactNode } from "react";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../store/store";
+
+const PrivateRoute = ({ children }: { children: ReactNode }) => {
+	const { userUid } = useSelector((state: RootState) => state.auth);
+
+	return userUid ? (
+		children
+	) : (
+		<Navigate
+			to={`${ROUTES.SIGNIN}`}
+			state={{
+				backgroundPath: location,
+				modalOpen: true,
+			}}
+			replace
+		/>
+	);
+};
+
+export default PrivateRoute;
