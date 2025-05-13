8d912ea231ced5e5088451ab7d6760b2fe36cc7e
Natalia Grishina <tata@MacBook-Air-Natalia.local>
2025-05-13 14:00:57 +0300

    style: добавила стили для второстепенной ссылки

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,14 +1,22 @@
 import type { ReactNode } from "react";
 import { Link } from "react-router-dom";
 import styles from "./link-button.module.scss";
+import clsx from "clsx";
+
+type LinkStyles = "primary" | "secondary";
 
 interface ILinkButtonProps {
 	to: string;
 	children: ReactNode;
+	style?: LinkStyles;
 }
 
-const LinkButton = ({ to, children }: ILinkButtonProps) => {
-	return <Link to={to} className={styles.link}>{children}</Link>;
+const LinkButton = ({ to, children, style = "primary" }: ILinkButtonProps) => {
+	return (
+		<Link to={to} className={clsx(styles.link, styles[`${style}`])}>
+			{children}
+		</Link>
+	);
 };
 
 export default LinkButton;
